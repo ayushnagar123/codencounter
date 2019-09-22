dfa1ad3d7ccc8af098bb53a703e19e13a78efdf3
“Frankenstien-hack” <sid15798@gmail.com>
2019-09-22 06:24:35 +0530

    6:30

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
-  }
+}
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -15,10 +15,10 @@ const nodemailer = require('nodemailer')
 const nhbs = require('nodemailer-express-handlebars')
 var port = process.env.PORT || 3000;
 var sessionStore = new MySQLStore({
-    host:'localhost',
-    database: 'naggaro',
-    user: 'ayush',
-    password: 'ayush'
+    host: 'localhost',
+    database: 'NAGGARO',
+    user: 'sid',
+    password: 'sid'
 });
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(urlencodedParser);
@@ -73,22 +73,21 @@ app.get('/vac', (req, res) => {
     res.render('vac')
 })
 
-app.post('/',(req,res)=>{
+app.post('/', (req, res) => {
     var email = req.body.email
-    var pass= req.body.password
-    var query='select * from registration where email="'+email+'" and password="'+pass+'"';
-    conn.query(query,(err,result)=>{
-        if(err) throw err;
+    var pass = req.body.password
+    var query = 'select * from registration where email="' + email + '" and password="' + pass + '"';
+    conn.query(query, (err, result) => {
+        if (err) throw err;
         console.log(result)
-        if(result){
+        if (result) {
             res.redirect('/login')
-        }
-        else{
+        } else {
             res.redirect('/');
         }
     })
 })
- 
+
 app.use(session({
     key: 'session_cookie_name',
     secret: 'session_cookie_secret',
@@ -97,14 +96,14 @@ app.use(session({
     saveUninitialized: false
 }));
 
-app.post('/signup',(req,res)=>{
+app.post('/signup', (req, res) => {
     var name = req.body.name;
     var age = req.body.age;
     var email = req.body.email;
     var phone = req.body.contact;
     var pass = req.body.password;
     var gen = req.body.gender
-    var query =`INSERT INTO registration(name,age,email,contact,password,gender) VALUES(
+    var query = `INSERT INTO registration(name,age,email,contact,password,gender) VALUES(
         '${name}',
         ${age},
         '${email}',
@@ -113,169 +112,84 @@ app.post('/signup',(req,res)=>{
         '${gen}'
     )`;
     conn.query(
-        query,function(err,results){
-            if(err){
+        query,
+        function(err, results) {
+            if (err) {
                 console.log(err)
                 res.redirect('/registration')
-            }
-            else{
+            } else {
                 console.log(results)
-                res.render('index',{
-                    name:name
+                res.render('index', {
+                    name: name
                 })
             }
         })
 })
 
-app.use('/city',userRoutes);
+app.use('/city', userRoutes);
 
-// app.get('/city/:id',(req,res)=>{
-//     var id = req.params.id;
-//     console.log(id);
-//     var query=`select * from info where City ='${id}' or PLACES='${id}';`;
-//     var city=`${id}`;
-//     // weather.setLang('it');
-//     // weather.setCity(`new york`);
-//     // weather.getTemperature(function(err, temp){
-//     //     console.log(temp);
-//     // });
-//     conn.query(query,function(err,cit){
-//         if(err){
-//             throw err;
-//         }
-//         else{ 
-//             city=cit[0];
-//         }
-//     })
-//     var linklist=[];
-//         var query=`select * from new_table where City ='${id}';`;
-//         conn.query(query,(err,link)=>{
-//         if(err){
-//             console.log(err);
-//         }
-//         else{
-//             var resultArray = Object.values(JSON.parse(JSON.stringify(link)))
-//             // linklist.push({link:link[0].link1})
-//             // linklist.push({link:link[0].link2})
-//             // linklist.push({link:link[0].link3})
-//             // linklist.push({link:link[0].link4})
-//             // linklist.push({link:link[0].link5})
-//             resultArray.forEach(function(v){ linklist.push(v) })
-            
-//         }
-//     })
-    
-//     var query=`select heading from info where City ='${id}' or PLACES='${id}';`;
-//     conn.query(
-//         query,function(err,explore){
-//             if(err){
-//                 console.log(err)
-//                 // res.redirect('/registration')
-//             }
-//             else{
-//                 query=`select food from info where City ='${id}' or PLACES='${id}';`;
-//                 conn.query(query,function(err,food){
-//                     if(err){
-//                         console.log(err)
-//                         // res.redirect('/registration')
-//                     }
-//                     else{
-//                         query=`select culture from info where City ='${id}' or PLACES='${id}';`;
-//                         conn.query(query,function(err,culture){
-//                             if(err){
-//                                 console.log(err)
-//                                 // res.redirect('/registration')
-//                             }
-//                             else{
-//                                 query=`select places_v from info where City ='${id}' or PLACES='${id}';`;
-//                                 conn.query(query,function(err,places){
-//                                     if(err){
-//                                         console.log(err)
-//                                         // res.redirect('/registration')
-//                                     }
-//                                     else{
-//                                         sender={explore:explore,food:food,culture:culture,places:places}
-//                                         console.log(explore.heading)
-//                                         res.render('city',{
-//                                             city:id,
-//                                             explore:sender['explore'],
-//                                             food:sender['food'],
-//                                             culture:culture[0],
-//                                             places:places[0],
-//                                             linklist:linklist
-//                                         })
-//                                     }
-//                                 })
-//                             }
-//                         })
-//                     }
-//                 })
-//             }
-//         })
-// })
-
-app.post('/mailme',(req,res)=>{
+app.post('/mailme', (req, res) => {
     let transporter = nodemailer.createTransport({
-        service:'gmail',
-        auth:{
-            user:process.env.EMAIL1,
-            pass:process.env.PASS
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL1,
+            pass: process.env.PASS
         }
     });
 
-  //step 2
-  let mailOptions = {
-      from:process.env.EMAIL1,
-      to:process.env.EMAIL2,
-      subject:'test mail',
-      text: 'Query sent from '+ req.body.Name+' '+ req.body.Email + '\n'+req.body.Message
-  };
+    //step 2
+    let mailOptions = {
+        from: process.env.EMAIL1,
+        to: process.env.EMAIL2,
+        subject: 'test mail',
+        text: 'Query sent from ' + req.body.Name + ' ' + req.body.Email + '\n' + req.body.Message
+    };
 
-  //step 3
-  transporter.sendMail(mailOptions,function(err,data){
-      if(err) console.log(err);
-      else console.log('email sent');
-  });
-  /*
+    //step 3
+    transporter.sendMail(mailOptions, function(err, data) {
+        if (err) console.log(err);
+        else console.log('email sent');
+    });
+    /*
 for demo and quick start place this code in your app.js or server.js
 please run 'npm install puretext --save' in terminal
 */
 
-const puretext = require('puretext');
-require('request');
+    //     const puretext = require('puretext');
+    //     require('request');
 
-let text = {
-    // To Number is the number you will be sending the text to.
-    toNumber: '+91-996-806-8330',
-    // From number is the number you will buy from your admin dashboard
-    fromNumber: '+919717504706',
-    // Text Content
-    smsBody: 'Sending SMS using Node.js',
-    //Sign up for an account to get an API Token
-    apiToken: 'testaccount'
-};
+    //     let text = {
+    //         // To Number is the number you will be sending the text to.
+    //         toNumber: '+91-996-806-8330',
+    //         // From number is the number you will buy from your admin dashboard
+    //         fromNumber: '+919717504706',
+    //         // Text Content
+    //         smsBody: 'Sending SMS using Node.js',
+    //         //Sign up for an account to get an API Token
+    //         apiToken: 'testaccount'
+    //     };
 
-puretext.send(text, function (err, response) {
-  if(err) console.log(err);
-  else console.log(response)
+    //     puretext.send(text, function(err, response) {
+    //         if (err) console.log(err);
+    //         else console.log(response)
+    //     })
+    //     res.redirect('/vac');
 })
-  res.redirect('/vac');
-})
 
-app.get('/logout',(req,res)=>{
+app.get('/logout', (req, res) => {
     session.end();
 })
 
-app.listen(port,(err)=>{
-    if(err) throw err;
+app.listen(port, (err) => {
+    if (err) throw err;
     console.log('connected')
     conn = mysql.createConnection({
-        host:'localhost',
-        database: 'naggaro',
-        user: 'ayush',
-        password: 'ayush'
+        host: 'localhost',
+        database: 'NAGGARO',
+        user: 'sid',
+        password: 'sid'
     })
-    if(conn) console.log('db connected');
-    if(!sessionStore) throw('seession error');
+    if (conn) console.log('db connected');
+    if (!sessionStore) throw ('seession error');
     console.log('session created')
 })
